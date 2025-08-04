9a1474c05703235c0d356082075e901f93b1fb29
Pritam Chaudhari <pritam.chaudhari@nitorinfotech.com>
2025-08-04 19:00:22 +0530

    Apppage modifications

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routes } from "../utils/routes";
 import { AuthProvider } from "./components/Auth/AuthProvider";
-import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/app/components/AppPage/AppPage.jsx
+++ src/app/components/AppPage/AppPage.jsx
@@ -1,13 +1,21 @@
+import Header from "../../components/Header/Header";
+import SideDrawer from "../../components/SideDrawer/SideDrawer";
+import Footer from "../../components/Footer/Footer";
 import PageBody from "./PageBody";
 import PageHeader from "./PageHeader";
 import "./AppPage.css";
 
 const AppPage = ({ title, description, children }) => {
   return (
-    <main id="app-body" className="app-body">
-      <PageHeader title={title} description={description} />
-      <PageBody>{children}</PageBody>
-    </main>
+    <div className="app">
+      <Header />
+      <SideDrawer />
+      <main id="app-body" className="app-body">
+        <PageHeader title={title} description={description} />
+        <PageBody>{children}</PageBody>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
